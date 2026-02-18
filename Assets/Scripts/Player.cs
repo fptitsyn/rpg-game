7b101da1135c3c814766fef8f51e62d869855d5d
NK <fedor.ptith@gmail.com>
2026-02-18 11:24:54 +0300

    Movement and almost camera

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,86 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float sensitivity;
+    [SerializeField] private float sprintMultiplier;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float distanceToGround;
+    
+    private InputAction _lookAction;
+    private InputAction _walkAction;
+    private InputAction _sprintAction;
+    private InputAction _jumpAction;
+
+    private const float Gravity = -9.81f;
+    
+    private CharacterController _controller;
+    private bool _grounded;
+    private Vector3 _velocity;
+    private float _moveSpeed;
+    private float _hp = 100;
+    
+    private void Start()
+    {
+        _lookAction = InputSystem.actions.FindAction("Look");
+        _walkAction = InputSystem.actions.FindAction("Move");
+        _sprintAction = InputSystem.actions.FindAction("Sprint");
+        _jumpAction = InputSystem.actions.FindAction("Jump");
+        
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Walk();
+        Look();
+    }
+
+    private void Walk()
+    {
+        Vector2 moveValue = _walkAction.ReadValue<Vector2>();
+        if (_sprintAction.IsPressed() && moveValue is { y: > 0, x: 0 })
+        {
+            _moveSpeed = sprintMultiplier * walkSpeed;
+        }
+        else
+        {
+            _moveSpeed = walkSpeed;
+        }
+        
+        if (_controller.isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+        
+        Vector3 move = transform.right * moveValue.x + transform.forward * moveValue.y;
+            
+        _controller.Move(move * (_moveSpeed * Time.deltaTime));
+            
+        _grounded = Physics.Raycast(transform.position, Vector3.down, distanceToGround);
+            
+        if (_jumpAction.IsPressed() && _grounded)
+        {
+            _grounded = false;
+            _velocity.y = jumpForce;
+        }
+            
+        _velocity.y += Gravity * Time.deltaTime;
+        _controller.Move(_velocity * Time.deltaTime);
+    }
+
+    private void Look()
+    {
+        Vector2 lookValue = _lookAction.ReadValue<Vector2>() * (sensitivity * Time.deltaTime);
+        
+        transform.Rotate(Vector3.up, lookValue.x);
+    }
+
+    private void ReceiveDamage(float damage)
+    {
+        
+    }
+}
